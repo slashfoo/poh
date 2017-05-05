ef129e70a0535e481842714c9bd5868c1a8c5f85
Jamiel Almeida <slashfoo@gmail.com>
2017-05-04 20:35:57 -0700

    expose poh module functions through package

---
+++ poh/__init__.py
@@ -0,0 +1,5 @@
+from poh import (print_execution_results,
+                 read_result_files,
+                 redirect_streams,
+                 remote_execute,
+                 run_poh)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='poh',
-    version='0.1.0',
+    version='0.1.1',
     description='ssh commands runner',
     long_description=long_description,
     url='https://github.com/slashfoo/poh',
